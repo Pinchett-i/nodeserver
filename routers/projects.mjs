6b7100734e3f07ce77d2b0b60b47225f91736123
Pinchett-i <sebastien.pinchetti@blue-planet.be>
2022-10-11 12:01:18 +0200

    Add Routes / Controllers / Views for Projects

---
+++ routers/projects.mjs
@@ -0,0 +1,33 @@
+import express from "express";
+import ProjectsController from "../controllers/projects_controller.mjs";
+var router = express.Router();
+
+router.get('/', function (request, response, next) {
+  ProjectsController.index(request, response)
+});
+
+router.get('/new', function (request, response, next) {
+  ProjectsController.newAction(request, response)
+});
+
+router.post('/', (request, response) => {
+  ProjectsController.create(request, response)
+})
+
+router.get('/:id/edit', (request, response) => {
+  ProjectsController.edit(request, response)
+})
+
+router.get('/:id', (request, response) => {
+  ProjectsController.show(request, response)
+})
+
+router.post('/:id', (request, response) => {
+  ProjectsController.update(request, response)
+})
+
+router.get('/:id/delete', (request, response) => {
+  ProjectsController.destroy(request, response)
+})
+
+export default router
