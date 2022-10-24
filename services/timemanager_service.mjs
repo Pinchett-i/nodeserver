fe7f4a3f84dd8fd00dc579655cd6e2374bef1492
Pinchett-i <sebastien.pinchetti@blue-planet.be>
2022-10-24 15:17:39 +0200

    Add `fetch_project_from_timemanager` to `TimeManagerService`

--- services/timemanager_service.mjs
+++ services/timemanager_service.mjs
@@ -1,19 +1,46 @@
-import time_registrations from '../fixtures/time_registration.json' assert { type: "json" }
 import config from "config";
 import https from 'https';
 
 class TimeManagerService {
   static async hours_for_project(project) {
-    let sum = 0;
     let times = await this.time_registrations_for_project(project)
-    times.forEach(time_registration => {
-      sum += time_registration.billable_hours
-    })
+    let sum = times.reduce((current_sum, time_registration) => {
+      return current_sum + time_registration.billable_hours
+    }, 0)
     return sum
   }
 
   static async time_registrations_for_project(project) {
-    return time_registrations // Fixture for now
+    return await this.fetch_project_time_registration_from_timemanager(project)
+  }
+
+  static async fetch_project_time_registration_from_timemanager(project) {
+    return new Promise((resolve, reject) => {
+      let req = https.get(this.request_options(project), response => {
+        resolve(this.parse_response(response));
+        req.on('error', (err) => {
+          reject(err);
+        });
+        req.end();
+      })
+
+    })
+  }
+
+  static request_options(project) {
+    return {
+      host: config.get('timemanager.host'),
+      path: `/projects/${project.id}/timeregistrations`,
+      headers: {
+        'HTTP_AUTHORIZATION': config.get('timemanager.api_key')
+      },
+      body: {
+        project: project,
+      }
+    }
+  }
+  static parse_response(response) {
+    return response
   }
 }
 
